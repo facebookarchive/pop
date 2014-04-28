a183467e58e3fd7785e7ba1e5d9cd860be2d485d
James Richard <james@weebly.com>
2014-04-28 10:13:27 -0700

    Removes extraneous NSLog statement

--- pop/POPAnimationTracer.mm
+++ pop/POPAnimationTracer.mm
@@ -132,7 +132,6 @@ static POPAnimationEvent *create_event(POPAnimationTracer *self, POPAnimationEve
   [_events addObject:event];
 
   if (_shouldLogAndResetOnCompletion) {
-    NSLog(@"events:%@", self.allEvents);
     [self reset];
   }
 }
