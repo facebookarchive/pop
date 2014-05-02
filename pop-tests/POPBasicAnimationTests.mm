cab40717de81f5eb8449b2cc63a3bd7dfde46e9d
Kimon Tsinteris <kimon@fb.com>
2014-05-02 15:51:53 -0700

    fix copyright notice

--- pop-tests/POPBasicAnimationTests.mm
+++ pop-tests/POPBasicAnimationTests.mm
@@ -1,10 +1,11 @@
-//
-//  POPBasicAnimationTests.m
-//  pop
-//
-//  Created by Kimon Tsinteris on 5/2/14.
-//  Copyright (c) 2014 Facebook. All rights reserved.
-//
+/**
+ Copyright (c) 2014-present, Facebook, Inc.
+ All rights reserved.
+
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <SenTestingKit/SenTestingKit.h>
 
@@ -29,17 +30,17 @@
 
   POPAnimationTracer *tracer = anim.tracer;
   [tracer start];
-  
+
   CALayer *layer = [CALayer layer];
   [layer pop_addAnimation:anim forKey:nil];
-  
+
   // run animation
   POPAnimatorRenderDuration(self.animator, self.beginTime, 3, 1.0/60.0);
 
   // verify write events
   NSArray *writeEvents = [tracer eventsWithType:kPOPAnimationEventPropertyWrite];
   STAssertTrue(writeEvents.count > 5, @"expected more write events %@", tracer.allEvents);
-  
+
   // assert final value
   POPAssertColorEqual((__bridge CGColorRef)anim.toValue, layer.backgroundColor);
 }
