d88c88947e233c679812ce1d96fb2b846abe95d9
jerome Morissard <morissardj@gmail.com>
2014-05-18 22:12:42 +0200

    addAnimation forObject using keyPath as the animation key

--- pop/POPAnimatorPrivate.h
+++ pop/POPAnimatorPrivate.h
@@ -50,6 +50,7 @@
  Funnel methods for category additions.
  */
 - (void)addAnimation:(POPAnimation *)anim forObject:(id)obj key:(NSString *)key;
+- (void)addAnimation:(POPAnimation *)anim forObject:(id)obj keyPath:(NSString *)keyPath;
 - (void)removeAllAnimationsForObject:(id)obj;
 - (void)removeAnimationForObject:(id)obj key:(NSString *)key;
 - (NSArray *)animationKeysForObject:(id)obj;
