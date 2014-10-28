7b0e96802d779091bb290b146a96a8b8af0ae1bf
b3ll <adam@adambell.me>
2014-10-28 19:19:18 -0400

    Remove use of embeddedframework

--- pop-ios/pop-ios.h
+++ pop-ios/pop-ios.h
@@ -1,10 +1,11 @@
-//
-//  pop-ios.h
-//  pop-ios
-//
-//  Created by Adam Bell on 10/28/14.
-//  Copyright (c) 2014 Facebook. All rights reserved.
-//
+/**
+ Copyright (c) 2014-present, Facebook, Inc.
+ All rights reserved.
+ 
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <UIKit/UIKit.h>
 
